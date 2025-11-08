703fe5046b53487b2acecd1b63ae24916fcf72a9
Nima <nimaginobilisalami@gmail.com>
2025-11-08 17:09:39 +0100

    skrevet kommentarer i GroqRequest

--- src/main/java/org/example/aiproject/dto/GroqRequest.java
+++ src/main/java/org/example/aiproject/dto/GroqRequest.java
@@ -8,15 +8,15 @@ Matcher Groq's API spec præcist
  */
 
 public record GroqRequest(
-        String model,
-        List<Message> messages,
-        double temperatur,
-        int max_tokens
+        String model, // modellen vi vælger at bruge
+        List<Message> messages, // liste af beskeden mellem llm og bruger
+        double temperature, // styrer llm'ens kreativitet - 0.0 til 2.0
+        int max_tokens // bestemmer længden på svaret fra llm
 ) {
 
     // Nested record for chat messages.
     public record Message(
-            String role,    // "bruger"
-            String content  // Beskedens indhold
+            String role,    // om det er "llm" eller "bruger"
+            String content  // beskedens indhold
     ) {}
 }
